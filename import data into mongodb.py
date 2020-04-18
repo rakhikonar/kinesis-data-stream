c15faa5f6220053132f77e5de19b987bd4683183
rakhikonar <63758828+rakhikonar@users.noreply.github.com>
2020-04-18 00:25:08 -0400

    Add files via upload

---
+++ import data into mongodb.py
@@ -0,0 +1,47 @@
+
+##Importing data into Mongo db
+
+import requests
+import boto3
+import json
+import botocore
+from pymongo import MongoClient
+import requests
+import json
+import pandas as pd
+
+
+
+AWSACCESSKEY="A***************"
+AWSSECRETKEY="*******************"
+AWSTOKEN="FwoGZXIvYXdzEEQaDH6nKPMu+l9tUgBk4SLDAezzp9Oc+p9TqHL1BEg9z3YbC0iR/0lrAbSap36GnHSUvDbwLTNAhT0COZmqAlNLAA8eXr5YePe3rxTXl5ZUtFytpslyHxy+JBRleCvh/4H9uqGrto+/bV2nQ/7TplDCEXpLY41Ws11yf84pgi3rfQfAZaZlN5axMRtaqqZ9PZM0s5P8y0u3xrfHotqqmztCu2QPQXCBLliGPW0f6QLW1M5Kz9h2QgWdF0/hMeCK7SsB3qGM+dkHGLfNVgYrco5vV5PTkyiau830BTIt6938ly6DHoAScjvMc21pNBI8LWhKMsbfav7mI+O6rXjTN+I2XEjtYaBVGgb8"
+
+session = boto3.Session(profile_name='default')
+
+# Taking the json file from s3 bucket
+
+s3 = boto3.client("s3",aws_access_key_id=AWSACCESSKEY, aws_secret_access_key=AWSSECRETKEY, aws_session_token=AWSTOKEN, region_name='us-east-1')
+#bucket = s3.Bucket('aws.project')
+object = s3.get_object(Bucket='aws.storage.project',Key='bestbuy.json')
+serializedObject = object['Body'].read().decode('utf-8')
+file_data = json.loads(serializedObject)
+#print(file_data)
+
+#Mongodb data import
+
+#All data collection
+
+client = MongoClient('localhost', 27017)
+db = client['bestbuydata_db']
+best_buy_data = db['best_buy']
+best_buy_data.insert_one(file_data)
+
+best_buy_data = json_normalize(d['products'])
+print(best_buy_data)
+#Products collection
+products = db['products']
+records_ = df.to_dict(orient = 'records')
+result = db.collection_products.insert_many(records_ )
+
+print("Data imported")
+client.close
